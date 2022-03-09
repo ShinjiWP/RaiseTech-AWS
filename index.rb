08c666e070f619fbe595dac30050b4cb7b1db37d
EC2 Default User <ec2-user@ip-172-31-4-249.ap-northeast-1.compute.internal>
2022-03-09 13:30:36 +0000

    202203092230 Rubyのループ処理など簡単なものから触った

--- index.rb
+++ index.rb
@@ -1,1 +1,36 @@
-puts "hello world!!!"
+# puts "hello world!!!"
+
+# aisatsu = "Hello World!!"
+# puts aisatsu
+
+# x = 0
+# for i in 1..10
+#     x += 1
+# end
+
+# puts x
+
+# def place(num)
+#     if num == 0 then
+#         print("引数が0なら偽")
+#         elsif num == 1 then
+#             print("引数が1なら正")
+#         end
+#     end
+    
+# print(place(1))
+# print(place(0)) 
+
+# array = [1,2,3,4,5]
+
+# for value in array
+#     if value == 2
+#         next
+#     end
+#     puts value
+# end
+
+todohuken = ["Tokyo", "Kanagawa", "Saitama"]
+for place in todohuken
+    puts place
+end
