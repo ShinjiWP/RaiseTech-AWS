6203a55f3dedc70936d86ebfd06fe1b9d0b26978
EC2 Default User <ec2-user@ip-172-31-4-249.ap-northeast-1.compute.internal>
2022-03-11 09:48:43 +0000

    202203111845 Rubyの基本メソッドの学習。JSや Pythonと同様に扱えそうな感触。

--- index.rb
+++ index.rb
@@ -30,7 +30,37 @@
 #     puts value
 # end
 
-todohuken = ["Tokyo", "Kanagawa", "Saitama"]
-for place in todohuken
-    puts place
-end
+# todohuken = ["Tokyo", "Kanagawa", "Saitama"]
+# for place in todohuken
+#     puts place
+# end
+
+
+# puts "hello world"
+# p "hello world"
+
+# p "hello".class
+# p 1.methods
+
+# puts 2*20
+
+# greeting = "Hello World"
+# puts greeting
+
+
+
+#連想配列　keyとvalue
+menu_list = {"らーめん" => "800円", "チャーハン" => "600円", "ビール" => "500円"}
+# puts menu_list["らーめん"]
+# puts menu_list["ビール"]
+
+# menu_list["らーめん"] = "1000円"
+# puts menu_list["らーめん"]
+
+menu_list.each{
+    |menu,price|
+    p "#{menu}は#{price}です"
+}
+
+p menu_list.length
+p menu_list.empty?
